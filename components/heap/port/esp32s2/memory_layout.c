901ec580d543863a9e74a0b29b5e70662ccabf15
Danny Wolf <wolfd@users.noreply.github.com>
2026-07-22 18:07:48 +0000

    heap: require explicit capability for SPIRAM

--- components/heap/port/esp32s2/memory_layout.c
+++ components/heap/port/esp32s2/memory_layout.c
@@ -42,7 +42,7 @@ const soc_memory_type_desc_t soc_memory_types[] = {
     { "IRAM", { MALLOC_CAP_EXEC|MALLOC_CAP_32BIT|MALLOC_CAP_INTERNAL, 0, 0 }, false, false},
     //Type 4: SPI SRAM data
     //TODO, in fact, part of them support EDMA, to be supported.
-    { "SPIRAM", { MALLOC_CAP_SPIRAM|MALLOC_CAP_DEFAULT, 0, MALLOC_CAP_8BIT|MALLOC_CAP_32BIT}, false, false},
+    { "SPIRAM", { MALLOC_CAP_SPIRAM, 0, MALLOC_CAP_8BIT|MALLOC_CAP_32BIT}, false, false},
     //Type 5: RTC Fast RAM
     { "RTCRAM", { MALLOC_CAP_RTCRAM, MALLOC_CAP_8BIT|MALLOC_CAP_DEFAULT, MALLOC_CAP_INTERNAL|MALLOC_CAP_32BIT }, false, false},
 };
